546bafefbcc96478772af6d12b3102fd3c077e19
Vik Singh <vik.singh@evidence.care>
2024-03-27 09:01:41 -0700

    Updated config file

--- glue/code-generator/generate_etl_views.py
+++ glue/code-generator/generate_etl_views.py
@@ -35,30 +35,35 @@ class Column:
     self.numeric_scale: int = numeric_scale
     self.udt_name: str = udt_name
     self.sf_data_type: str = sf_data_type
-  
-    if self.data_type == "uuid" or self.data_type.startswith("character"):
-      self.sf_data_type = 'text'
-    elif self.data_type.startswith("timestamp"):
+    
+    if self.data_type.startswith("timestamp"):
       self.sf_data_type = "timestamp"
     elif self.data_type == "numeric":
       self.sf_data_type = f"numeric ({self.numeric_precision}, {self.numeric_scale})"
-  
+    else:
+      self.sf_data_type = 'text'
+      
 class Table:
   def __init__(self, name: str):
     self.name: str = name
     self.load_method: str = ""
     self.columns: List[Column] = []
+    self.target_table: str = ""
+    self.load_source_data_clients = [] 
+    self.dynamic_table_clients = [] 
 
   def add_column(self, column: Column) -> None:
     self.columns.append(column)
 
 class Schema:
-  def __init__(self, database: str, name: str, target_database: str, target_schema: str ):
+  def __init__(self, database: str, name: str, target_database: str, target_schema: str, load_source_data_clients = [], dynamic_table_clients = [] ):
     self.name: str = name
     self.database: str = database
     self.target_schema: str = target_schema
     self.target_database: str = target_database
     self.tables: List[Table] = []
+    self.load_source_data_clients = load_source_data_clients
+    self.dynamic_table_clients = dynamic_table_clients 
   
   def __str__(self):
         return f"{self.name}"
@@ -69,12 +74,12 @@ class Schema:
   
 config = load_config('glue\\code-generator\\config.ini')
 
-def find_or_create_schema(schemas: List[Schema], database: str, schema_name: str, target_database: str, target_schema: str) -> Schema:
+def find_or_create_schema(schemas: List[Schema], database: str, schema_name: str, target_database: str, target_schema: str, load_source_data_clients = [], dynamic_table_clients = []) -> Schema:
     for schema in schemas:
         if schema.name == schema_name and schema.database == database:
             return schema
     # If not found, create a new Schema and add it to the list
-    new_schema = Schema(database, schema_name, target_database, target_schema)
+    new_schema = Schema(database, schema_name, target_database, target_schema, load_source_data_clients, dynamic_table_clients)
     schemas.append(new_schema)
     return new_schema
 
@@ -126,99 +131,125 @@ def get_load_method (columns):
     load_method = "merge"
   return load_method
 
-def get_schema_data(config: AppConfig, pg_database, environment): 
-  schema_names = config.load_data.schema_names.split(',')
-  schemas: List[Schema] = []
-  tunnel, conn = create_db_connection(environment)
+# def get_schema_data(config: AppConfig, pg_database, environment): 
+#   schema_names = config.load_data.schema_names.split(',')
+#   schemas: List[Schema] = []
+#   tunnel, conn = create_db_connection(environment)
   
-  try:
-    with conn.cursor() as cursor:
-      for schema in schema_names:
-        # Execute the query to get all tables in the schema
-        query = sql.SQL("SELECT table_name FROM information_schema.tables WHERE table_schema = %s")
-        cursor.execute(query, [schema])
-        tables = cursor.fetchall()
-        schema = Schema(pg_database, schema)
-        for (table_name,) in tables:
-          table = Table(table_name)
-          cursor.execute(sql.SQL(
-              "SELECT column_name, data_type, numeric_precision, numeric_scale, udt_name FROM information_schema.columns WHERE table_schema = %s AND table_name = %s"),
-              [schema.name, table_name]
-          )
-          columns = cursor.fetchall()
-          # Loop through columns and add details to the table_info
-          for column in columns:
-            column_obj = Column(*column)
-            table.add_column(column_obj)
+#   try:
+#     with conn.cursor() as cursor:
+#       for schema in schema_names:
+#         # Execute the query to get all tables in the schema
+#         query = sql.SQL("SELECT table_name FROM information_schema.tables WHERE table_schema = %s")
+#         cursor.execute(query, [schema])
+#         tables = cursor.fetchall()
+#         schema = Schema(pg_database, schema)
+#         for (table_name,) in tables:
+#           table = Table(table_name)
+#           cursor.execute(sql.SQL(
+#               "SELECT column_name, data_type, numeric_precision, numeric_scale, udt_name FROM information_schema.columns WHERE table_schema = %s AND table_name = %s"),
+#               [schema.name, table_name]
+#           )
+#           columns = cursor.fetchall()
+#           # Loop through columns and add details to the table_info
+#           for column in columns:
+#             column_obj = Column(*column)
+#             table.add_column(column_obj)
           
-          table.load_method = get_load_method(columns)
-          # Append the table info to the schema_data list
-          schema.add_table(table)
-        schemas.append(schema)
-  except psycopg2.Error as e:
-    print(f"Database query failed: {e.pgerror}")
-    print(f"Detailed error: {e.diag.message_detail}")
-  finally:
-    conn.close()
-    tunnel.stop()
-  return schemas
+#           table.load_method = get_load_method(columns)
+#           # Append the table info to the schema_data list
+#           schema.add_table(table)
+#         schemas.append(schema)
+#   except psycopg2.Error as e:
+#     print(f"Database query failed: {e.pgerror}")
+#     print(f"Detailed error: {e.diag.message_detail}")
+#   finally:
+#     conn.close()
+#     tunnel.stop()
+#   return schemas
     
     
 def get_schema_data_config(pg_database, environment): 
-  csv_file_path = f"{config.outputdir.pg_sf_mapping_config_path}\\pg_to_sf_mapping.csv"  
+  csv_file_path = f"{config.outputdir.pg_sf_mapping_config_path}\\pg_to_sf_mapping_revamped.csv"  
   schema_list: List[Schema] = []
   tunnel, conn = create_db_connection(environment)
   with open(csv_file_path, mode='r', newline='') as file:
-      reader = csv.DictReader(file)  # Assuming the CSV has headers
-      try:
-        with conn.cursor() as cursor:
-          for row in reader:
-            # Define the columns you want to use for distinctness
-            if (row['client_name'] == 'generic' and row['load_source_data']) == 'y' and row['generate_code'] == 'y':
-              schema_name = row['source_schema']
-              table_name = row['source_table']          
-              load_method = row['load_method']
-              target_schema = row['target_schema']
-              target_database = row['target_database']
-              
-              schema: Schema = find_or_create_schema(schema_list, pg_database, schema_name, target_database, target_schema)
-              table = Table(table_name)
+    reader = csv.DictReader(file)  # Assuming the CSV has headers
+    try:
+      with conn.cursor() as cursor:
+        for row in reader:
+          # Define the columns you want to use for distinctness
+          if row['source_database'] == pg_database and row['generate_code'] == 'y':
+            schema_name = row['source_schema']
+            target_schema = row['target_schema']
+            target_database = row['target_database']
+            load_source_data_clients = row['load_source_data_clients'].split('|')
+            dynamic_table_clients = row['dynamic_table_clients'].split('|')
+            schema: Schema = find_or_create_schema(schema_list, pg_database, schema_name, target_database, target_schema, load_source_data_clients, dynamic_table_clients)
+            
+            if row['source_table'] == "*":
               cursor.execute(sql.SQL(
-                  "SELECT column_name, data_type, numeric_precision, numeric_scale, udt_name FROM information_schema.columns WHERE table_schema = %s AND table_name = %s"),
-                  [schema_name, table_name]
+                "SELECT table_name FROM information_schema.tables WHERE table_schema = %s"),
+                [schema_name]
               )
-              columns = cursor.fetchall()
-              # Loop through columns and add details to the table_info
-              for column in columns:
-                column_obj = Column(*column)
-                table.add_column(column_obj)
-              
-              table.load_method = load_method
+              tables = cursor.fetchall()
+              for tbl in tables:
+                table = schema_data_table_columns(schema.name, tbl[0], cursor, load_source_data_clients, dynamic_table_clients)                
+                schema.add_table(table)
+            else:
+              table_name = row['source_table']          
+              table = schema_data_table_columns(schema.name, table_name, cursor, load_source_data_clients, dynamic_table_clients)
               schema.add_table(table)
-      except psycopg2.Error as e:
-        print(f"Database query failed: {e.pgerror}")
-        print(f"Detailed error: {e.diag.message_detail}")
-      finally:
-        conn.close()
-        if tunnel != None:
-          tunnel.stop()
+            
+    except psycopg2.Error as e:
+      print(f"Database query failed: {e.pgerror}")
+      print(f"Detailed error: {e.diag.message_detail}")
+    finally:
+      conn.close()
+      if tunnel != None:
+        tunnel.stop()
   return schema_list       
+
+
+def schema_data_table_columns(schema_name: str, table_name: str, cursor, load_source_data_clients, dynamic_table_clients):
+  table = Table(table_name)
+  table.dynamic_table_clients = dynamic_table_clients
+  table.load_source_data_clients = load_source_data_clients
+  cursor.execute(sql.SQL(
+      "SELECT column_name, data_type, numeric_precision, numeric_scale, udt_name FROM information_schema.columns WHERE table_schema = %s AND table_name = %s"),
+      [schema_name, table_name]
+  )
+  columns = cursor.fetchall()
+  # Loop through columns and add details to the table_info
+  for column in columns:
+    column_obj = Column(*column)
+    table.add_column(column_obj)
+  load_method = get_load_method(columns)
+  table.load_method = load_method
+  return table
 
 
 def generate_db_objects_using_config(environment, pg_database):
   schema_data = get_schema_data_config(pg_database, environment)
-  generate_pg_objects(schema_data)
-  generate_sf_tables(schema_data)
-  generate_sf_dynamic_tables(schema_data)
+  
+  # for schema in schema_data:
+  #   print (f"Schema: {schema.name}")
+  #   for table in schema.tables:
+  #     print (f"Table: {table.name}, load_source_clients: {table.load_source_data_clients}, dynamic_table_clients: {table.dynamic_table_clients}")
+      
+  
+  # generate_pg_objects(schema_data)
+  # generate_sf_tables(schema_data)
+  # generate_sf_dynamic_tables(schema_data)
   generate_sf_build_script(schema_data)
     
-def generate_db_objects_using_schemas(environment, pg_database, sf_dynamic_table_db, sf_dynamic_table_schema):
-  schema_data = get_schema_data(pg_database, environment)
-  generate_pg_objects(schema_data)
-  generate_sf_tables(schema_data)
-  generate_sf_dynamic_tables(schema_data)
-  generate_sf_build_script(schema_data)
-  generate_config_csv(schema_data, sf_dynamic_table_db, sf_dynamic_table_schema)
+# def generate_db_objects_using_schemas(environment, pg_database, sf_dynamic_table_db, sf_dynamic_table_schema):
+#   schema_data = get_schema_data(pg_database, environment)
+#   generate_pg_objects(schema_data)
+#   generate_sf_tables(schema_data)
+#   generate_sf_dynamic_tables(schema_data)
+#   generate_sf_build_script(schema_data)
+#   # generate_config_csv(schema_data, sf_dynamic_table_db, sf_dynamic_table_schema)
 
 def generate_pg_objects(schema_list: List[Schema]):
   os.makedirs(config.outputdir.pg_etl_views_path, exist_ok=True)  
@@ -270,10 +301,9 @@ def generate_pg_objects(schema_list: List[Schema]):
       grant_file.write(pg_grant_script.upper())
     
 def generate_sf_tables(schema_list: List[Schema]):
-  clients = config.load_data.client_names.split(',')
-  for client in clients:   
-    for schema in schema_list:     
-      for table in schema.tables:
+  for schema in schema_list:     
+    for table in schema.tables:
+      for client in table.load_source_data_clients:   
         sf_table_output_dir = f"{config.outputdir.sf_staging_tables_path}\\data_ingestion\\schemas\\greyhavens_{client}_{schema.name}\\tables"  
         os.makedirs(sf_table_output_dir, exist_ok=True)  # Create the directory if it doesn't exist
         columns_list = ', '.join([f"\n\t{col.name} {col.sf_data_type}"for col in table.columns])
@@ -294,10 +324,8 @@ def generate_sf_tables(schema_list: List[Schema]):
           file_path = os.path.join(sf_table_output_dir, f"stage_{table.name}.sql")
           with open(file_path, "w") as file:
             file.write(stage_table_sql.upper())
-      
+    
 def generate_sf_dynamic_tables(scheme_list: List[Schema]):
-  clients = config.load_data.client_names.split(',')
-  
   for schema in scheme_list:
     for table in schema.tables:
       sf_dynamic_table_output_dir = f"{config.outputdir.sf_dynamic_tables_path}\\{schema.target_database}\\schemas\\{schema.target_schema}\\tables"  
@@ -313,7 +341,7 @@ def generate_sf_dynamic_tables(scheme_list: List[Schema]):
       dynamic_table_sql_create = f"CREATE DYNAMIC TABLE IF NOT EXISTS {dynamic_table_name} \nTARGET_LAG = 'DOWNSTREAM' \nWAREHOUSE = ETL_WH \nAS"
       dynamic_table_sql_select = ""
       file_path = os.path.join(sf_dynamic_table_output_dir, f"{table.name}.sql")
-      for idx, client in enumerate(clients):
+      for idx, client in enumerate(table.dynamic_table_clients):
         staging_table_name = f"data_ingestion.greyhavens_{client}_{schema.name}.{table.name}"
         if idx > 0:
           dynamic_table_sql_select = f"{dynamic_table_sql_select}\n\tUNION"
@@ -329,16 +357,24 @@ def generate_sf_build_script(schema_list: List[Schema]):
     sf_build_script_dir = config.outputdir.sf_build_script_path
     os.makedirs(sf_build_script_dir, exist_ok=True)  # Create the directory if it doesn't exist
     build_script_file_path = os.path.join(sf_build_script_dir, "build.sql")
+    build_script = []
+    for schema in schema_list:
+      for client in schema.load_source_data_clients:
+        build_script.append(f"-f data_ingestion\\schemas\\{schema.database}_{client}_{schema.name}\\{schema.database}_{client}_{schema.name}.sql `\n")
+        for table in schema.tables:
+          build_script.append(f"-f data_ingestion\\schemas\\{schema.database}_{client}_{schema.name}\\tables\\{table.name}.sql `\n")
+          if (table.load_method == "merge"):
+            build_script.append(f"-f data_ingestion\\schemas\\{schema.database}_{client}_{schema.name}\\tables\\stage_{table.name}.sql `\n")
+    
+    # Dynamic Tables
+    for schema in schema_list:
+      for table in schema.tables:
+        if len(table.dynamic_table_clients) > 0:
+          build_script.append(f"-f {schema.target_database}\\schemas\\{schema.target_schema}\\tables\\{table.name}.sql `\n")  
+            
     with open(build_script_file_path, "w") as file:
-      for client in clients:
-        for schema in schema_list:
-          build_sql = f"-f data_ingestion\\schemas\\{schema.database}_{client}_{schema.name}\\{schema.database}_{client}_{schema.name}.sql `\n"
-          file.write(build_sql) 
-          for table in schema.tables:
-            build_sql = f"-f data_ingestion\\schemas\\{schema.database}_{client}_{schema.name}\\tables\\{table.name}.sql `\n"
-            if (table.load_method == "merge"):
-              build_sql = f"{build_sql}-f data_ingestion\\schemas\\{schema.database}_{client}_{schema.name}\\tables\\stage_{table.name}.sql `\n"
-            file.write(build_sql) 
+      file.writelines(build_script)
+      
 
 # def generate_config_json(schema_data: List[Schema], sf_db_name, sf_schema_name):
 #   # Directory where the SQL files will be saved
@@ -394,29 +430,29 @@ def generate_sf_build_script(schema_list: List[Schema]):
 #             print("No changes made.")
       
 
-def generate_config_csv(schema_data: List[Schema], sf_db_name, sf_schema_name):
-  conif_file_dir = config.outputdir.pg_sf_mapping_config_path
-  os.makedirs(conif_file_dir, exist_ok=True)  # Create the directory if it doesn't exist
-  clients = config.load_data.client_names.split(',')
+# def generate_config_csv(schema_data: List[Schema], sf_db_name, sf_schema_name):
+#   conif_file_dir = config.outputdir.pg_sf_mapping_config_path
+#   os.makedirs(conif_file_dir, exist_ok=True)  # Create the directory if it doesn't exist
+#   clients = config.load_data.client_names.split(',')
   
-  csv_file_name = f"{conif_file_dir}\\pg_to_sf_mapping_new.csv"  
-  headers = "client_name", "source_database", "source_schema", "source_table", "source_etl_view", "target_database", "target_schema", "target_table", "load_method", "load_source_data", "refresh_dynamic_table", "description"
-  data = []
+#   csv_file_name = f"{conif_file_dir}\\pg_to_sf_mapping_new.csv"  
+#   headers = "client_name", "source_database", "source_schema", "source_table", "source_etl_view", "target_database", "target_schema", "target_table", "load_method", "load_source_data", "refresh_dynamic_table", "description"
+#   data = []
   
-  for client in clients:    
-    for schema in schema_data:
-      for table in schema.tables:
-        pg_view_name = f"{schema.name}_{table.name}_vw"
-        row = [client,schema.database,schema.name,table.name,pg_view_name,sf_db_name,sf_schema_name,table.name,table.load_method,'y','y','']
-        data.append(row)
-  sorted_rows = sorted(data, key=lambda x: (x[0], x[1],x[2],x[3]))
+#   for client in clients:    
+#     for schema in schema_data:
+#       for table in schema.tables:
+#         pg_view_name = f"{schema.name}_{table.name}_vw"
+#         row = [client,schema.database,schema.name,table.name,pg_view_name,sf_db_name,sf_schema_name,table.name,table.load_method,'y','y','']
+#         data.append(row)
+#   sorted_rows = sorted(data, key=lambda x: (x[0], x[1],x[2],x[3]))
   
-  with open(csv_file_name, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
-    writer.writerows(sorted_rows)
+#   with open(csv_file_name, mode='w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(headers)
+#     writer.writerows(sorted_rows)
 
-  print("Config file generated successfully")
+#   print("Config file generated successfully")
 
 # def generate_config_element(client_name, database, schema_name,  sf_dynamic_table_db, sf_dynamic_table_schema, table_name, etl_view, load_method):
 #   return {
@@ -440,7 +476,7 @@ def generate_config_csv(schema_data: List[Schema], sf_db_name, sf_schema_name):
 
 
 # read_config_file()
-generate_db_objects_using_config('dev', 'greyhavens')
+generate_db_objects_using_config('local', 'greyhavens')
 # generate_db_objects_using_schemas('local', 'greyhavens', 'core', 'codes')
 
 
